3ee126bd757f6727a3b83980c4f7f7a5e1ba77c5
Salina Bermudez <144279649+sbermudez01@users.noreply.github.com>
2023-10-01 16:16:56 -0700

    Update GenerateImage.m

--- Functions/GenerateImage.m
+++ Functions/GenerateImage.m
@@ -1,7 +1,11 @@
 function [B,X] = GenerateImage(Ac,Ar, varargin)
+% GenerateImage given X and operators KP form
+% Note all images are scaled to max of Btrue since Btrue scaled to max 1
+% varargin{5} X : array of size m by n which is the image to blur, or
+%                this references an image file and not an array
 m=size(Ac,1);
 n=size(Ar,1);
-if nargin >= 3 
+if nargin >= 3 % read in an image
     if ischar(varargin{1})
         X = imread(varargin{1});
         X = double(im2gray(X));
@@ -10,7 +14,7 @@ if nargin >= 3
         X = varargin{1};
     end
     [mX,nX]=size(X);
-    if m<mX 
+    if m<mX % try to take from center of image
         dm=floor((mX-m)/2);
         X=X(dm+1:dm+m,:);
     end
@@ -19,7 +23,7 @@ if nargin >= 3
         X=X(:,dn+1:n+dn);
     end
 end
-if nargin<3 ||  isempty(X)
+if nargin<3 ||  isempty(X) %standard image of circles and squares
     X = zeros(m,n);
     I = round(m/5):round(3*m/5);
     J = round(n/5):round(3*n/5);
