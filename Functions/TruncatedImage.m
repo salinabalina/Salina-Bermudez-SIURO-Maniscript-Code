a1b38b64e3c0b24dd8bec086dbd0e5da1d676fdd
Salina Bermudez <144279649+sbermudez01@users.noreply.github.com>
2023-10-01 16:17:11 -0700

    Update TruncatedImage.m

--- Functions/TruncatedImage.m
+++ Functions/TruncatedImage.m
@@ -1,11 +1,11 @@
 function Xtrunc=TruncatedImage(Ac,Ar,B,kind)
 [Uc,Sc,Vc]=svd(Ac);
 [Ur,Sr,Vr]=svd(Ar);
-Sc=diag(Sc); 
-Sr=diag(Sr); 
-KPSVs=Sc*Sr'; 
+Sc=diag(Sc); %extract into diagonal matrix
+Sr=diag(Sr); %extract into diagonal matrix
+KPSVs=Sc*Sr'; %KP singular values page 50 HNO book
 [m,n]=size(KPSVs);
-[sortKP, sortKPind]=sort(KPSVs(:),'descend');
+[sortKP, sortKPind]=sort(KPSVs(:),'descend'); % index into sorted values of the KPs
 CoefB=(Uc'*B*Ur);
 KPSVpinv=zeros(size(KPSVs(:)));
 indKP=floor(kind);
